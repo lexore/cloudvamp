98eedda02fbebb16a86bd1d5967dcec4554797be
Igor Isaenko <iisaenko@ivi.ru>
2019-04-08 17:34:24 +0300

    actualize remotes to 5.8

--- opennebula/im/cloudvamp-probes.d/cloudvamp.rb
+++ opennebula/im/cloudvamp-probes.d/cloudvamp.rb
@@ -1,20 +1,7 @@
 #!/usr/bin/env ruby
 
 # -------------------------------------------------------------------------- #
-#                                                                            #
-# This is a modified version of the kvm.rb file from OpenNebula KVM IM,      #
-# version 4.8. This file include modifications to adapt it to CloudVAMP.     #
-# Modifications are concentrated in the first part of the code, although     #
-# other parts may also have been modified.                                   #
-#                                                                            #
-# -------------------------------------------------------------------------- #
-
-# -------------------------------------------------------------------------- #
-# Original file                                                              #
-# Copyright 2002-2014, OpenNebula Project (OpenNebula.org), C12G Labs        #
-#                                                                            #
-# CloudVAMP modifications                                                    #
-# Copyright 2015, Universitat Politecnica de Valencia                        #
+# Copyright 2002-2019, OpenNebula Project, OpenNebula Systems                #
 #                                                                            #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may    #
 # not use this file except in compliance with the License. You may obtain    #
@@ -34,11 +21,43 @@ def print_info(name, value)
     puts "#{name}=#{value}"
 end
 
-vmsinfo_text = `../../vmm/cloudvamp/poll --kvm -t`
+######
+#  First, get all the posible info out of virsh
+#  TODO : use virsh freecell when available
+######
+
+ENV['LANG'] = 'C'
+ENV['LC_ALL'] = 'C'
+
+nodeinfo_text = `virsh -c qemu:///system nodeinfo`
+exit(-1) if $?.exitstatus != 0
+
+nodeinfo_text.split(/\n/).each{|line|
+    if     line.match('^CPU\(s\)')
+        $total_cpu   = line.split(":")[1].strip.to_i * 100
+    elsif  line.match('^CPU frequency')
+        $cpu_speed   = line.split(":")[1].strip.split(" ")[0]
+    elsif  line.match('^Memory size')
+        $total_memory = line.split(":")[1].strip.split(" ")[0]
+    end
+}
+
+######
+#  CPU
+######
+vmstat = `vmstat 1 2`
+$free_cpu = $total_cpu * ((vmstat.split("\n").to_a.last.split)[14].to_i)/100
+$used_cpu = $total_cpu - $free_cpu
+
+######
+#  MEMORY
+######
+
+vmsinfo_text = `../../vmm/cloudvamp/poll -t`
 exit(-1) if $?.exitstatus != 0
 
 # Set the percentage of packed memory dedicated for overcommiting
-$O = 1.0
+$O = 0.7
 
 original_vmsinfo_text = vmsinfo_text
 $virtual_mem = 0
@@ -63,55 +82,42 @@ vmsinfo_text.split(/\n/).each{|line|
 	end
 }
 
-######
-#  First, get all the posible info out of virsh
-#  TODO : use virsh freecell when available
-######
+memory = `cat /proc/meminfo`
+meminfo = Hash.new()
+memory.each_line do |line|
+  key, value = line.split(':')
+  meminfo[key] = /\d+/.match(value)[0].to_i
+end
 
-nodeinfo_text = `virsh -c qemu:///system nodeinfo`
-exit(-1) if $?.exitstatus != 0
+$total_memory = meminfo['MemTotal']
 
-nodeinfo_text.split(/\n/).each{|line|
-    if     line.match('^CPU\(s\)')
-        $total_cpu   = line.split(":")[1].strip.to_i * 100
-    elsif  line.match('^CPU frequency')
-        $cpu_speed   = line.split(":")[1].strip.split(" ")[0]
-    elsif  line.match('^Memory size')
-        $total_memory = line.split(":")[1].strip.split(" ")[0]
-    end
-}
+$used_memory = meminfo['MemTotal'] - meminfo['MemFree'] - meminfo['Buffers'] - meminfo['Cached']
+$free_memory = $total_memory - $used_memory
 
 ######
-#   for everything else, top & proc
-#####
+#  INTERFACE
+######
 
-NETINTERFACE = "eth|bond|em|p[0-9]+p[0-9]+"
+NETINTERFACE = [
+  'eth[0-9]+',
+  'ib[0-9]+',
 
-top_text=`top -bin2`
-exit(-1) if $?.exitstatus != 0
+  # --- BIOS derived names:
+  'em[0-9]+(_[0-9]+)?',
+  'p[0-9]+p[0-9]+(_[0-9]+)?',
 
-top_text.gsub!(/^top.*^top.*?$/m, "") # Strip first top output
-
-top_text.split(/\n/).each{|line|
-    if line.match('^%?Cpu')
-        line[7..-1].split(",").each{|elemento|
-            temp = elemento.strip.split(/[% ]/)
-            if temp[1]=="id"
-            idle = temp[0]
-            $free_cpu = idle.to_f * $total_cpu.to_f / 100
-            $used_cpu = $total_cpu.to_f - $free_cpu
-                break
-            end
-
-        }
-    end
-}
-
-$total_memory = `free -k|grep "Mem:" | awk '{print $2}'`
-tmp=`free -k|grep "Mem:"|awk '{print $3 " " $4}'`.split
-
-$used_memory=tmp[0]
-$free_memory=tmp[1]
+  # --- Systemd naming - type of names for en*:
+  # o<index>[n<phys_port_name>|d<dev_port>] — on-board device index number
+  # s<slot>[f<function>][n<phys_port_name>|d<dev_port>] — hotplug slot index number
+  # x<MAC> — MAC address
+  # [P<domain>]p<bus>s<slot>[f<function>][n<phys_port_name>|d<dev_port>] — PCI geographical location
+  # a<vendor><model>i<instance> — Platform bus ACPI instance id
+  'eno[0-9]+(n[0-9a-zA-Z]+|d[0-9]+)?',
+  'ens[0-9]+(f[0-9]+)?(n[0-9a-zA-Z]+|d[0-9]+)?',
+  'enx[0-9a-fA-F]{12}',
+  'en(P[0-9]+)?p[0-9]+s[0-9]+(f[0-9]+)?(n[0-9a-zA-Z]+|d[0-9]+)?',
+  'ena[0-9a-zA-Z]+i[0-9]+',
+].join('|')
 
 net_text=`cat /proc/net/dev`
 exit(-1) if $?.exitstatus != 0
@@ -120,7 +126,7 @@ $netrx = 0
 $nettx = 0
 
 net_text.split(/\n/).each{|line|
-    if line.match("^ *#{NETINTERFACE}")
+    if line.match("^ *(#{NETINTERFACE}):")
         arr   = line.split(":")[1].split(" ")
         $netrx += arr[0].to_i
         $nettx += arr[8].to_i
@@ -128,16 +134,18 @@ net_text.split(/\n/).each{|line|
 }
 
 print_info("HYPERVISOR","kvm")
+
 print_info("TOTALCPU",$total_cpu)
 print_info("CPUSPEED",$cpu_speed)
+
 print_info("TOTALMEMORY",$total_memory.to_f + ($O * $virtual_mem.to_f))
 print_info("PACKEDMEMORY",$virtual_mem)
+
 print_info("USEDMEMORY",$used_memory)
 print_info("FREEMEMORY",$free_memory)
+
 print_info("FREECPU",$free_cpu)
 print_info("USEDCPU",$used_cpu)
+
 print_info("NETRX",$netrx)
 print_info("NETTX",$nettx)
-
-# Remove or coment the following line if you are setting permission for execution for poll.sh
-puts original_vmsinfo_text
